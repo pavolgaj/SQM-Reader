74a6c54ab604ba2f5b138eab76739359817eb586
Pavol Gajdoš <pavol1gajdos@gmail.com>
2020-02-11 12:43:30 +0100

    Add files via upload

--- sqm_reader.py
+++ sqm_reader.py
@@ -1,37 +1,204 @@
+import sys
+import os
+import tkinter as tk
+import tkinter.ttk as ttk
 import time
+import asyncio
+import threading
+import math
 import serial
+import serial.tools.list_ports
 
-port='COM9'
-t0=0
-dt=60
-
-com=serial.Serial(port)
-com.baudrate=115200
-print('Init')
-time.sleep(10)
-print('Init finished')
-
-i=0
-loop=True
-while loop:
-    if time.time()-t0>dt:
-        t0=time.time()
-        com.write(b'rx\r')
-        print('reading')
-        time.sleep(5)
-        sqm=com.readline().decode().strip()
-        ##        while not ans==b'\n':
-##            print(ans)
-##            sqm+=ans
-##            ans=str(com.read())
-##            print('reading')
-##            break
-        print('finished')
-        #sqm=com.read()       #r,-09.42m,0000005915Hz,0000000000c,0000000.000s, 027.0C -> r,mpsas,freq,period,per,temp 
-        cas=time.strftime('%Y_%m_%d %H:%M:%S',time.localtime(t0))
-        print(cas,sqm)
-        i+=1
-        loop=(i<3)
-    time.sleep(1)
-com.close() 
+def read1():
+    '''read one data'''
+    t0=time.time()
+    if com in globals():
+        com.write(b'rx\r')            
+        time.sleep(5)  #wait for completing measurements
+        ans=com.readline().decode().strip()
+    else: ans='r, 19.42m,0000005915Hz,0000000000c,0000000.000s, 027.0C'  #TODO: development    
+    data=ans.split(',')     #r,-09.42m,0000005915Hz,0000000000c,0000000.000s, 027.0C -> r,mpsas,freq,period,per,temp 
+    t=time.strftime('%Y_%m_%d %H:%M:%S',time.localtime(t0))
+    mpsas=float(data[1][:-1])   #mpsas
+    nelm=round(7.93-5*math.log10(math.pow(10,4.316-(mpsas/5.))+1),2) #nelm
+    temp=float(data[-1][:-1])  #temperature in C
     
+    mpsasVar.set(mpsas)
+    nelmVar.set(nelm)
+    tempVar.set(temp)
+    timeVar.set(t)
+       
+    if saveVar.get(): 
+        name=pathVar.get()+'sqm_'+time.strftime('%Y_%m_%d')+'.dat'
+        if os.path.isfile(name): f=open(name,'a')
+        else:
+            f=open(name,'w')
+            f.write('Date Time MPSAS NELM Temp(C)\n')
+        f.write('%s %5.2f %5.2f %4.1f\n' %(t,mpsas,nelm,temp))
+        f.close()
+    return t0
+
+async def read_loop():
+    '''loop for repeated reading'''
+    t0=0
+    dt=60*dtVar.get()  #interval in sec
+    while loopTest:
+        if time.time()-t0>dt: t0=read1()
+        await asyncio.sleep(0.1)
+    
+def stop():
+    '''stop repeated reading'''
+    global loopTest
+    loopTest=False
+    
+def reading():
+    '''start repeated reading'''
+    global loop,loopTest
+    loopTest=True
+    loop=asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(read_loop())
+    
+def init():
+    '''init serial COM port'''
+    global com
+    com=serial.Serial(portVar.get())
+    com.baudrate=baudVar.get()
+    
+def exit():
+    global root
+    stop()
+    try: com.close() 
+    except NameError: pass  #not used
+    root.destroy()
+    root=None
+
+root=tk.Tk()
+root.geometry('400x350')
+root.title('SQM Reader')
+root.protocol('WM_DELETE_WINDOW',exit)
+
+portVar=tk.StringVar(root)
+baudVar=tk.IntVar(root)
+dtVar=tk.DoubleVar(root)
+pathVar=tk.StringVar(root)
+saveVar=tk.BooleanVar(root)
+mpsasVar=tk.DoubleVar(root)
+nelmVar=tk.DoubleVar(root)
+tempVar=tk.DoubleVar(root)
+timeVar=tk.StringVar(root)
+
+saveVar.set(0)
+baudVar.set(115200)
+dtVar.set(1)
+
+Label1=tk.Label(root)
+Label1.place(relx=0.04,rely=0.03,height=21,width=30)
+Label1.configure(text='Port')
+
+TCombobox1=ttk.Combobox(root)
+TCombobox1.place(relx=0.13,rely=0.03,relheight=0.06,relwidth=0.29)
+TCombobox1.configure(values=sorted([x.device for x in serial.tools.list_ports.comports()]))
+TCombobox1.configure(textvariable=portVar)
+TCombobox1.configure(width=137)
+
+Label2=tk.Label(root)
+Label2.place(relx=0.46,rely=0.03,height=21,width=63)
+Label2.configure(text='Baudrate')
+
+TCombobox2=ttk.Combobox(root)
+TCombobox2.place(relx=0.65,rely=0.03,relheight=0.06,relwidth=0.31)
+TCombobox2.configure(values=[9600,14400,19200,38400,56000,57600,115200,128000,256000])
+TCombobox2.configure(textvariable=baudVar)
+TCombobox2.configure(width=147)
+
+Button1=tk.Button(root)
+Button1.place(relx=0.36,rely=0.11,height=29,width=127)
+Button1.configure(text='Init')
+Button1.configure(width=127)
+Button1.configure(command=init)
+
+Label3=tk.Label(root)
+Label3.place(relx=0.04,rely=0.21,height=21,width=78)
+Label3.configure(text='Save to file')
+
+Checkbutton1=tk.Checkbutton(root)
+Checkbutton1.place(relx=0.21,rely=0.21,relheight=0.06,relwidth=0.06)
+Checkbutton1.configure(justify=tk.LEFT)
+Checkbutton1.configure(variable=saveVar)
+
+Entry1=tk.Entry(root)
+Entry1.place(relx=0.04,rely=0.29,height=23,relwidth=0.92)
+Entry1.configure(background='white')
+Entry1.configure(width=436)
+Entry1.configure(textvariable=pathVar)
+
+Button2=tk.Button(root)
+Button2.place(relx=0.04,rely=0.4,height=29,width=59)
+Button2.configure(text='Read')
+Button2.configure(command=read1)
+
+Button4=tk.Button(root)
+Button4.place(relx=0.19,rely=0.4,height=29,width=100)
+Button4.configure(text='Read every')
+Button4.configure(command=lambda: threading.Thread(target=reading).start())
+
+Entry2=tk.Entry(root)
+Entry2.place(relx=0.44,rely=0.4,height=23,relwidth=0.16)
+Entry2.configure(background='white')
+Entry2.configure(width=76)
+Entry2.configure(textvariable=dtVar)
+
+Label4=tk.Label(root)
+Label4.place(relx=0.63,rely=0.4,height=21,width=57)
+Label4.configure(text='minutes')
+
+Button3=tk.Button(root)
+Button3.place(relx=0.8,rely=0.4,height=29,width=55)
+Button3.configure(text='Stop')
+Button3.configure(command=stop)
+
+Label5=tk.Label(root)
+Label5.place(relx=0.06,rely=0.56,height=33,width=85)
+Label5.configure(font=('',18))
+Label5.configure(text='MPSAS')
+
+Entry3=tk.Entry(root)
+Entry3.place(relx=0.3,rely=0.56,height=33,relwidth=0.67)
+Entry3.configure(background='white')
+Entry3.configure(font=('',18))
+Entry3.configure(width=316)
+Entry3.configure(textvariable=mpsasVar)
+
+Label6=tk.Label(root)
+Label6.place(relx=0.06,rely=0.68,height=21,width=41)
+Label6.configure(text='NELM')
+
+Entry4=tk.Entry(root)
+Entry4.place(relx=0.3,rely=0.7,height=23,relwidth=0.65)
+Entry4.configure(background='white')
+Entry4.configure(width=306)
+Entry4.configure(textvariable=nelmVar)
+
+Label7=tk.Label(root)
+Label7.place(relx=0.06,rely=0.78,height=21,width=88)
+Label7.configure(text='Temperature')
+
+Entry5=tk.Entry(root)
+Entry5.place(relx=0.32,rely=0.78,height=23,relwidth=0.62)
+Entry5.configure(background='white')
+Entry5.configure(width=296)
+Entry5.configure(textvariable=tempVar)
+
+Label8=tk.Label(root)
+Label8.place(relx=0.06,rely=0.88,height=21,width=37)
+Label8.configure(text='Time')
+
+Entry6=tk.Entry(root)
+Entry6.place(relx=0.3,rely=0.88,height=23,relwidth=0.65)
+Entry6.configure(background='white')
+Entry6.configure(width=306)
+Entry6.configure(textvariable=timeVar)
+
+
+tk.mainloop()
